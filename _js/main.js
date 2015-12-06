77ab62cb068bd7798536362a3870e45028154530
Cameron Hurd <me@cameronhurd.com>
2015-12-06 17:56:19 -0500

    grunt => gulp

--- _js/main.js
+++ _js/main.js
@@ -1,11 +1,3 @@
-/*!
- * avgrund 0.1
- * http://lab.hakim.se/avgrund
- * MIT licensed
- *
- * Copyright (C) 2012 Hakim El Hattab, http://hakim.se
- */
-
 $(function(){
   
   /*
--- js/src/avgrund.js
+++
@@ -1,7 +0,0 @@
-/*!
- * avgrund 0.1
- * http://lab.hakim.se/avgrund
- * MIT licensed
- *
- * Copyright (C) 2012 Hakim El Hattab, http://hakim.se
- */
