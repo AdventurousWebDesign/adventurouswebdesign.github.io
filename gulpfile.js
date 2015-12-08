fd124aed15f5eebca844046f62616499cdbb09cc
Cameron Hurd <me@cameronhurd.com>
2015-12-07 19:17:42 -0500

    getting super sassc

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
-var gulp      = require('gulp');
-var concat    = require('gulp-concat');
-var rename    = require('gulp-rename');
-var uglify    = require('gulp-uglify');
+var gulp         = require('gulp');
+var concat       = require('gulp-concat');
+var rename       = require('gulp-rename');
+var uglify       = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var buffer       = require('vinyl-buffer');
+var gutil        = require('gulp-util');
+var plumber      = require('gulp-plumber');
+var rename       = require("gulp-rename");
+var run          = require('gulp-run');
+
+require('es6-promise').polyfill();
 
 var globalConfig = {
   css:     'css',
@@ -41,9 +49,25 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest(globalConfig.js_dest));
 });
 
+
+gulp.task('sassc', function () {
+  gulp.src('css/adventurous.scss', { buffer: false })
+    .pipe(plumber(function(err) {
+        gutil.beep();
+        var errorTxt = err.message +'\n\n'+ err.source;
+        gutil.log(gutil.colors.red(errorTxt));
+    }))
+    .pipe(run('sassc -s', {verbosity: 1}))
+    .pipe(rename(function (path) { path.extname = ".css"; }))
+    .pipe(buffer())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('css/'));
+});
+
 // Watch files
 gulp.task('watch', function () {
   gulp.watch(globalConfig.js + '/*.js', ['scripts']);
+  gulp.watch([globalConfig.sass + '/*.scss', 'css/*'], ['sassc']);
 });
 
 // Default task
